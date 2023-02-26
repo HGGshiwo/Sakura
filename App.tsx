b0a4fc8e8b5ec00228f2697c081fc7067e6b5239
HGGshiwo <3190102788@zju.edu.cn>
2023-02-26 16:25:02 +0800

    增加了tab bar

--- App.tsx
+++ App.tsx
@@ -7,13 +7,24 @@
 
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {StatusBar, useColorScheme} from 'react-native';
+import {StatusBar, useColorScheme, Text} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import VideoPage from './scene/VideoPage';
 import HomePage from './scene/HomePage';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {
+  faBook,
+  faC,
+  faHouse,
+  faPalette,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import {faYoutube} from '@fortawesome/free-brands-svg-icons';
+import { Pressable } from 'react-native/Libraries/Components/Pressable/Pressable';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -22,6 +33,58 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const Tab = createBottomTabNavigator();
+  const icons = {
+    //Animation
+    home: faYoutube,
+    User: faUser,
+    Novel: faBook,
+    Comic: faPalette,
+  };
+
+  const texts = {
+    home: '番剧',
+    User: '我的',
+    Novel: '小说',
+    Comic: '漫画',
+  };
+
+  const TabPage = () => {
+    return (
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let icon = icons[route.name as keyof typeof icons];
+            // You can return any component that you like here!
+            return (
+              <FontAwesomeIcon
+                icon={icon}
+                size={18}
+                color={focused ? 'deeppink' : 'grey'}
+              />
+            );
+          },
+          headerShown: false,
+          tabBarLabel: ({focused, color, position}) => {
+            return (
+              <Text
+                style={{paddingBottom: 8, fontSize: 10, color: focused ? 'deeppink' : 'grey'}}>
+                {texts[route.name as keyof typeof texts]}
+              </Text>
+            );
+          },
+        })}>
+        <Tab.Screen name="home" component={HomePage} />
+        <Tab.Screen name="Novel" component={UserStackScreen} />
+        <Tab.Screen name="Comic" component={UserStackScreen} />
+        <Tab.Screen name="User" component={UserStackScreen} />
+      </Tab.Navigator>
+    );
+  };
+
+  const UserStackScreen = () => {
+    return <></>;
+  };
   const Stack = createNativeStackNavigator();
 
   return (
@@ -32,8 +95,10 @@ function App(): JSX.Element {
       />
       <NavigationContainer>
         <GestureHandlerRootView style={{flex: 1}}>
-          <Stack.Navigator initialRouteName="home" screenOptions={{headerShown: false}}>
-            <Stack.Screen name="home" component={HomePage} />
+          <Stack.Navigator
+            initialRouteName="home"
+            screenOptions={{headerShown: false}}>
+            <Stack.Screen name="home" component={TabPage} />
             <Stack.Screen name="video" component={VideoPage} />
           </Stack.Navigator>
         </GestureHandlerRootView>
